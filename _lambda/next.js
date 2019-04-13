d56bd61e7fae9982b7c25dd5ac6278edb589fbe2
Max Böck <hello@mxb.at>
2019-04-13 17:32:07 +0200

    add redirect rules

--- _lambda/next.js
+++ _lambda/next.js
@@ -2,6 +2,7 @@ import { redirect, getNext } from './common/utils'
 
 exports.handler = function(event, context, callback) {
     const { host } = event.headers
+    console.log(event.headers)
     const next = getNext(host)
 
     callback(null, redirect(next))
